6183eb64d70e546184e95e9ebc33da7517440e77
Michal Dobija <mic132mic@gmail.com>
2016-10-16 17:36:53 +0200

    ListGraph and improvements.

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,12 @@
  */
 public class Main {
     public static void main(String[] args) {
-        MatrixGraph<String, Integer> graph = new MatrixGraph<>();
+        MatrixGraph<Integer, String> graph = new MatrixGraph<>(Integer.class, String.class);
         graph.addVertex(1);
         graph.addVertex(2);
-        graph.removeVertex(1);
+        graph.addEdge("1to2", 1, 2, false);
+
+        Integer[] list = graph.getAdjacentVertices(1);
+        System.out.println(list[0]);
     }
 }
